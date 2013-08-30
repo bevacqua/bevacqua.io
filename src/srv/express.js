54b99efd5498cc5bab3b69525360eddef4ffe2e6
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-08-30 16:40:36 -0300

    moved folders around

--- src/srv/express.js
+++ src/srv/express.js
@@ -2,8 +2,11 @@
 
 var express = require('express'),
     logger = require('./lib/logger'),
+    routing = require('./routing.js'),
     app = express(),
     port = 3000;
+
+routing(app.router);
 
 app.locals.settings['x-powered-by'] = false;
 app.use(express.bodyParser());
