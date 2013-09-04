9dd14a12ce8f12bcf4a990afeb9cef0f19cdf60d
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-09-04 15:25:02 -0300

    more commenting on nconf use

--- env/index.js
+++ env/index.js
@@ -2,9 +2,18 @@
 
 var nconf = require('nconf');
 var path = require('path');
+var user = path.join(__dirname, 'private/user.json');
 var dev = path.join(__dirname, 'private/dev.json');
 var defaults = path.join(__dirname, 'defaults.json');
 
-nconf.argv().env().file(dev).file(defaults);
+// order of precedence:
 
+// argv     - process arguments
+// env      - environment-specific configuration
+// user     - user-provided values, if any
+// dev      - dev-environment settings, if any
+// defaults - default values if they weren't overwritten
+nconf.argv().env().file(user).file(dev).file(defaults);
+
+// expose global conf convenience method
 global.conf = module.exports = nconf.get.bind(nconf);
--- env/secure/README.markdown
+++ env/secure/README.markdown
@@ -3,3 +3,5 @@
 These values are encrypted using a `.pem` key that isn't distributed as part of the repository. The configuration present in these files pertains to API credentials, secure passwords, and the like. In production, environment variables are preferred.
 
 The upside of this approach is that you can distribute changes to the configuration values safely through `git`, and decrypt the updated version using the same `.pem` key you already have, making the process a bit less tedious.
+
+Note that you shouldn't be changing values to `dev.json` if you're not going to update the `.pemjson` version. You should use the `private/user.json` file, instead.
