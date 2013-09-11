664137c729030161d2333436cf00785e5daa10e5
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-09-10 22:02:37 -0300

    link icons

--- build/cfg/assets/img.js
+++ build/cfg/assets/img.js
@@ -12,16 +12,19 @@ module.exports = {
     sprite: function(type, short){
         return {
             src: 'src/client/img/sprite/' + type + '/**/*.{png,jpg,gif}',
-            destImg: 'bin/public/img/sprite/' + type + '.jpg',
+            destImg: 'bin/public/img/sprite/' + type + '.png',
             destCSS: 'bin/.tmp/sprite/' + type + '.css',
-            imgPath: '/img/sprite/' + type + '.jpg',
+            imgPath: '/img/sprite/' + type + '.png',
             cssOpts: {
                 cssClass: function (item) {
                     var prefix = short ? short + '-' : '';
                     return '.' + prefix + item.name + ':before';
                 }
             },
-            engine: 'gm'
+            engine: 'gm',
+            imgOpts: {
+                format: 'png'
+            }
         };
     }
 };
