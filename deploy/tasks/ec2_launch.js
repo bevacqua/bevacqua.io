898d8f6bd692161e1f591d2f04202c1732771dab
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-09-14 18:29:52 -0300

    fixed stupid issue in ec2_launch task

--- deploy/tasks/ec2_launch.js
+++ deploy/tasks/ec2_launch.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var util = require('util');
-
 module.exports = function(grunt){
 
     grunt.registerTask('ec2_launch', function(name){
-        var tasks = util.format('ec2_create_keypair:%s ec2_run:%s', name, name);
-        grunt.task.run(tasks);
+
+        grunt.task.run([
+            'ec2_create_keypair:' + name,
+            'ec2_run:' + name
+        ]);
     });
 };
