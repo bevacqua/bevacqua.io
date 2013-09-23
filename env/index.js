e0dbb3fbfb03963fcd0c68b34352374a655f8bb6
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-09-23 16:23:12 -0300

    better approach to defaults

--- env/index.js
+++ env/index.js
@@ -6,27 +6,27 @@ var path = require('path');
 var util = require('util');
 var nconf = require('nconf');
 var moment = require('moment');
-var defaults = require('./defaults.json');
 var pkg = require_cwd('package.json', __dirname);
 
 nconf.argv();
 nconf.env();
-nconf.defaults(defaults);
 
-var env = nconf.get('NODE_ENV');
+var env = nconf.get('NODE_ENV') || 'development';
 var loading = util.format('%s - Loading configuration for %s...', moment().format(), env);
 
 process.stdout.write(loading);
 
 file('user');
-file(env); // environment-specific configuration
+file(env);
+file('defaults', false);
 
 nconf.set('BUILD_VERSION', pkg.version);
 
 console.log('done');
 
-function file (name) {
-    var filename = util.format('private/%s.json', name);
+function file (name, secure) {
+    var location = secure === false ? '.' : 'private';
+    var filename = util.format('%s/%s.json', location, name);
     var filepath = path.join(__dirname, filename);
 
     nconf.file(name, filepath);
