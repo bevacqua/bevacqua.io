b8db90b6a8ccc23b6930dd45797852ab5f96851e
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-09-23 09:57:12 -0300

    env coloring

--- build/tasks/env.js
+++ build/tasks/env.js
@@ -1,12 +1,18 @@
 'use strict';
 
+var chalk = require('chalk');
+
 module.exports = function(grunt){
     grunt.registerTask('env', function(){
         grunt.log.writeln('Pulling environment variables and overrides...');
 
         var env = conf();
-        var envJson = JSON.stringify(env, null, 3);
 
-        grunt.log.writeln(envJson);
+        for (var key in env) {
+            var property = chalk.magenta(key);
+            var value = JSON.stringify(env[key], null, 2);
+
+            grunt.log.writeln('%s: %s', property, value);
+        }
     });
 };
