d0d7e72b6440e21fc37e88871e771f0f09fb6886
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-09-25 18:30:06 -0300

    feat: /code points to hub/bevacqua

--- src/srv/controllers/backend/transparencyController.js
+++ src/srv/controllers/backend/transparencyController.js
@@ -3,7 +3,12 @@
 var controller = module.exports = require('../Controller.js')();
 
 controller.registerRoutes = function(app){
+    app.get('/code', controller.getCode);
     app.get('/source', controller.getSource);
+};
+
+controller.getCode = function(req, res){
+    res.redirect('https://github.com/bevacqua');
 };
 
 controller.getSource = function(req, res){
