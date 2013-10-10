ab7737bcf2ab906b086ad476bcc913c0b31db6ce
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-10-10 19:01:35 -0300

    stuff

--- env/globals.js
+++ env/globals.js
@@ -8,11 +8,11 @@ var cwd = path.resolve(__dirname, '..');
 global.conf = nconf.get.bind(nconf);
 global.conf.cwd = cwd;
 
+global.logger = require('../src/lib/logger');
+
 global.require_cwd = function(file){
     var absolute = path.join(cwd, file);
     return require(absolute);
 };
 
 moment.defaultFormat = 'Do HH:mm:ss';
-
-global.logger = require('../src/lib/logger');
--- src/lib/logger/transports.js
+++ src/lib/logger/transports.js
@@ -8,26 +8,31 @@ var stdout = logger.transports.Console;
 
 logger.remove(stdout);
 
-add(stdout, conf('CONSOLE_ENABLED'), {
+add(stdout, 'CONSOLE_ENABLED', {
     timestamp: timestamps,
     colorize: true,
     level: conf('CONSOLE_LEVEL')
 });
 
-add(pushover, conf('PUSHOVER_ENABLED'), {
+add(pushover, 'PUSHOVER_ENABLED', {
     level: conf('PUSHOVER_LEVEL'),
     userKey: conf('PUSHOVER_USER_KEY'),
     token: conf('PUSHOVER_API_TOKEN')
 });
 
-add(papertrail, conf('PAPERTRAIL_ENABLED'), {
+add(papertrail, 'PAPERTRAIL_ENABLED', {
     host: conf('PAPERTRAIL_HOST'),
     port: conf('PAPERTRAIL_PORT'),
     level: conf('PAPERTRAIL_LEVEL')
 });
 
 function add (transport, enabled, options) {
-    if (enabled) { logger.add(transport, options); }
+    var on = conf(enabled);
+    if (on) { logger.add(transport, options); }
+
+    var name = enabled.split('_')[0].toLowerCase();
+
+    console.log(timestamps(), '-', name, 'transport', on ? 'enabled' : 'off');
 }
 
 function timestamps(){
