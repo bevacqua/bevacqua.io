22c0d4c054e2a3d6d339cef251063338775a2261
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-11-05 08:44:21 -0300

    switching pemcrypt version

--- build/cfg/task/environment.js
+++ build/cfg/task/environment.js
@@ -2,28 +2,51 @@
 
 var path = require('path');
 var util = require('util');
-
 var settings = {
     pem_gen: {},
     pem_encrypt: {},
     pem_decrypt: {}
 };
 
-function add (keyName) {
-    var keyFile = util.format('env/private/%s.pem', keyName);
-    var pem = path.join(conf.cwd, keyFile);
+function addGenerator (task, pem) {
+    settings.pem_gen[task] = { pem: pem };
+}
 
-    settings.pem_gen[keyName] = { pem: pem };
-    settings.pem_encrypt[keyName] = settings.pem_decrypt[keyName] = {
+function addPemcrypt (task, pem, store, storeName, raw, secure) {
+    settings.pem_encrypt[task] = settings.pem_decrypt[task] = {
         pem: pem,
-        pemstore: 'env/secure/' + keyName,
-        rawstore: 'env/private/' + keyName
+        pemstore: store + '/secure/' + storeName,
+        rawstore: store + '/private/' + storeName,
+        private: raw,
+        secure: secure
     };
 }
 
-add('grunt');
-add('development');
-add('edge');
-add('production');
+function addConfig (keyName) {
+    var taskName = keyName + '_config';
+    var keyFile = util.format('env/private/%s.pemkey', keyName);
+    var pem = path.join(conf.cwd, keyFile);
+
+    addGenerator(taskName, pem);
+    addPemcrypt(taskName, pem, 'env', keyName);
+}
+
+function addDeploy (keyName) {
+    var taskName = keyName + '_deploy';
+    var keyFile = util.format('deploy/private/%s.pemkey', keyName);
+    var pem = path.join(conf.cwd, keyFile);
+
+    addGenerator(taskName, pem);
+    addPemcrypt(taskName, pem, 'deploy', keyName, '.pem', '.pemsecure');
+    addPemcrypt(taskName + '_pub', pem, 'deploy', keyName, '.pem.pub', '.pubsecure');
+}
+
+addConfig('grunt');
+addConfig('development');
+addConfig('edge');
+addConfig('production');
+
+addDeploy('edge');
+addDeploy('production');
 
 module.exports = settings;
