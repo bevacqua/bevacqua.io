dabc40371099c5afb62b9cc39e004ffea88f7910
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-08-28 08:13:14 +0200

    ref slides new talk

--- src/srv/controllers/view/talksController.js
+++ src/srv/controllers/view/talksController.js
@@ -5,4 +5,5 @@ var controller = module.exports = new (require('../ViewController.js'))('talks')
 controller.registerRoutes = function(app){
     app.get('/talks', controller.getView('/landing'));
     app.get('/talks/frontend-ops', controller.getView('frontend-ops'));
+    app.get('/talks/browserify-all-the-things', controller.getView('browserify-all-the-things'));
 };
