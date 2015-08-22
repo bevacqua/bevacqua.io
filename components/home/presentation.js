adc882496231617ede3f0ae03e11b25dde362869
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-08-22 10:54:05 -0300

    fix up attributes

--- components/home/presentation.js
+++ components/home/presentation.js
@@ -8,7 +8,7 @@ export default class Presentation extends React.Component {
         speakerdeck && <script data-id={speakerdeck.id} data-ratio={speakerdeck.ratio} src='//speakerdeck.com/assets/embed.js' className='speakerdeck-embed' async />
       }
       {
-        youtube && <iframe src={youtube} width='560' height='315' frameborder='0' allowfullscreen></iframe>
+        youtube && <iframe src={youtube} width='560' height='315' frameBorder='0' allowFullScreen></iframe>
       }
     </section>
   }
