eb73eaadadfb13161d9a41a35139fe9db282e416
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-08-24 00:34:43 -0300

    casing

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import {Route, DefaultRoute} from 'react-router';
-import App from './components/app';
+import App from './components/App';
 import HomeIndex from './components/home/index';
 import Talks from './components/talks/talks';
 import OpenSource from './components/opensource/opensource';
